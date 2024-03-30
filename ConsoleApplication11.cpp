2d31cfe633cffdd0028f892d4bef92de9c1eeb3d
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2024-03-30 21:34:57 +0200

    Add files via upload

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//Задание 1
+//bool Pred(string& val1,string& val2)
+//{
+//	return val1.size() < val2.size();
+//}
+//
+//
+//int main()
+//{
+//	vector<string> obj = { "Hello","Hi","friend","good" };
+//
+//	sort(obj.begin(), obj.end(), Pred);
+//
+//	for (auto str : obj)
+//	{
+//		cout << str << " ";
+//	}
+//
+//}
+
+
+
+
+
+
+
+//Задание 2
+//struct Func
+//{
+//    bool operator()(string& val1, string& val2)
+//    {
+//        return val1.size() < val2.size();
+//    }
+//};
+//
+//int main() {
+//    vector<string> obj = { "Hello","Hi" };
+//
+//    sort(obj.begin(), obj.end(), Func());
+//
+//    for (auto str : obj)
+//    {
+//        cout << str << " ";
+//    }
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
